5037530543711fea01133eba25db0d811d4096d1
Padraic Heberlein <padraicheberlein@gmail.com>
2022-06-21 21:47:35 -0400

    Updated user model

--- Evidex/Models/UserModel.cs
+++ Evidex/Models/UserModel.cs
@@ -7,15 +7,20 @@ namespace Evidex.Models
     public class UserModel
     {
         [Key]
+        [StringLength(100)]
+        [Display(Name = "User Name")]
         public string UserName { get; set; }
-        
+
+        [Required]
         [DataType(DataType.DateTime)]
+        [Display(Name = "Signed Up At")]
         public DateTime SignedUpAt { get; set; }
-        
+
+        [Required]
+        public UserType Type { get; set; }
+
         public IdentityUserToken<string> Token { get; }
 
         public ProfileModel Profile { get; set; }
-
-        public UserType Type { get; set; }
     }
 }
